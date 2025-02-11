dd9d6294fb5a440b4c09b69f3cdddccf14c9d098
rianlucas <rianluccas08@gmail.com>
2025-02-11 18:01:00 -0300

    refactor: rename property

--- brocker/internal/market/entity/order.go
+++ brocker/internal/market/entity/order.go
@@ -9,7 +9,7 @@ type Order struct {
 	Price         float64
 	OrderType     string
 	Status        string
-	Transaction   []*Transaction
+	Transactions  []*Transaction
 }
 
 func NewOrder(orderId string, investor *Investor, asset *Asset, price float64, shares int, orderType string) *Order {
@@ -22,6 +22,6 @@ func NewOrder(orderId string, investor *Investor, asset *Asset, price float64, s
 		Price:         price,
 		OrderType:     orderType,
 		Status:        "OPEN",
-		Transaction:   []*Transaction{},
+		Transactions:  []*Transaction{},
 	}
 }
